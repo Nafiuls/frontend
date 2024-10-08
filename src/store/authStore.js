5e60a0132a8732d8e6c5b9122b63faf500718aed
Nafiuls <snafiul700@gmail.com>
2024-10-08 18:50:35 +0600

    finished

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import axios from 'axios';
-const API_URL = `https://auth-project-steel.vercel.app/`;
+const API_URL = `https://backend-rho-sable.vercel.app/api/auth`;
 axios.defaults.withCredentials = true;
 
 export const useAuthStore = create((set) => ({
